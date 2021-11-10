29d296ac90da628912198179eb8c0f076884cb93
Lizzie Anthony <lizziewrab@me.com>
2021-11-10 17:46:03 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let imput = document.querySelector("#new-task-description").value
+    handle(imput)
+  })
+})
+
+function handle(newTaskDescription){
+  console.log(newTaskDescription)
+  let li = document.createElement('li')
+  li.textContent = newTaskDescription
+  let list = document.querySelector("#tasks")
+  list.appendChild(li)
+  console.log(li)
+}
